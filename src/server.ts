7377b8a2265132c3b28e8ebae23497c81c392b6b
DE-Uehara <r.uehara@defide-ix.com>
2025-08-07 21:00:22 +0900

    cors設定

--- src/server.ts
+++ src/server.ts
@@ -34,7 +34,10 @@ app.use(express.json());
 
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: {origin: "*"},
+  cors: {
+    origin: ["https://sushi-peace.web.app", "http://localhost:5173"],
+    methods: ["GET", "POST"],
+  },
 });
 
 // Dependencies
